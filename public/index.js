bf20c10b0b5f8e3595f33ad5249414a3627a4783
tthuc396 <dichthuc@gmail.com>
2016-10-24 18:30:01 -0400

    added private key and node file

---
+++ public/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+var counter = 0;
+app.get('/', function (req, res) {
+    res.send('Hello World has been said ' + counter + ' times!');
+    counter++;
+});
+
+app.listen(port, function () {
+    console.log('Example app listening on port' + port);
+});
